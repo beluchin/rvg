c9cb2a5e07a70cae85d30256c526ff93a4279c05
Abel Quiros <beluchin@gmail.com>
2022-09-15 11:27:31 +0100

    renamed test

--- src/test/java/aq/rvg/RandomTest.java
+++ src/test/java/aq/rvg/RandomTest.java
@@ -1,57 +1,53 @@
 package aq.rvg;
 
-import com.google.common.reflect.TypeToken;
 import aq.helpers.java.either.Either;
+import com.google.common.reflect.TypeToken;
 import lombok.Value;
 import lombok.val;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static aq.rvg.Functional.supplierOfRandom;
+import static aq.rvg.Operational.random;
 import static aq.rvg.Operational.randomBoolean;
+import static org.assertj.core.api.Assertions.assertThat;
 
-final class SupplierOfRandomTest {
+final class RandomTest {
     @Test
     void _1_basic_type() {
-        assertThat(supplierOfRandom(new TypeToken<Integer>() { }, Config.empty()).get())
+        assertThat(random(new TypeToken<Integer>() { }, Config.empty()))
                 .isNotZero();
-        assertThat(supplierOfRandom(new TypeToken<String>() { }, Config.empty()).get())
+        assertThat(random(new TypeToken<String>() { }, Config.empty()))
                 .isNotNull();
     }
 
     @Test
     void _2_user_defined() {
-        val value = supplierOfRandom(new TypeToken<WithoutTypeArgs>() { },
-                                     Config.empty())
-                .get();
+        val value = random(new TypeToken<WithoutTypeArgs>() { },
+                           Config.empty());
 
         assertThat(value.i).isNotZero();
     }
 
     @Test
     void _3_user_defined_with_type_args() {
-        val value = supplierOfRandom(new TypeToken<WithTypeArgs<Integer>>() { },
-                                     Config.empty())
-                .get();
+        val value = random(new TypeToken<WithTypeArgs<Integer>>() { },
+                           Config.empty());
 
         assertThat(value.t).isNotZero();
     }
 
     @Test
     void _4_arbitrary_nesting() {
-        val value = supplierOfRandom(new TypeToken<WithTypeArgs<WithTypeArgs<Integer>>>() { },
-                                     Config.empty())
-                .get();
+        val value = random(new TypeToken<WithTypeArgs<WithTypeArgs<Integer>>>() { },
+                           Config.empty());
 
         assertThat(value.t.t).isNotZero();
     }
 
     @Test
     void _5_multiple_constructor_args() {
-        val value = supplierOfRandom(new TypeToken<WithConstructorWithMultipleArgs<Integer>>() { },
-                                     Config.empty())
-                .get();
+        val value = random(new TypeToken<WithConstructorWithMultipleArgs<Integer>>() { },
+                           Config.empty());
 
         assertThat(value.i).isNotZero();
         assertThat(value.t).isNotZero();
@@ -81,16 +77,15 @@ final class SupplierOfRandomTest {
                           val typeArg0 = tt.resolveType(Either.class.getTypeParameters()[0]);
                           val typeArg1 = tt.resolveType(Either.class.getTypeParameters()[1]);
                           return randomBoolean()
-                                  ? Either.first(Operational.random(typeArg0, c))
-                                  : Either.second(Operational.random(typeArg1, c));
+                                  ? Either.first(random(typeArg0, c))
+                                  : Either.second(random(typeArg1, c));
                       })
                 .build();
 
         @Test
         void _1_special_constructor() {
-            val value = supplierOfRandom(new TypeToken<Either<Integer, String>>() { },
-                                         config)
-                    .get();
+            val value = random(new TypeToken<Either<Integer, String>>() { },
+                               config);
 
             value.apply(i -> assertThat(i).isNotZero(),
                         s -> assertThat(s).isNotNull());
@@ -98,9 +93,8 @@ final class SupplierOfRandomTest {
 
         @Test
         void _2_special_constructor_nested() {
-            val value = supplierOfRandom(new TypeToken<WithTypeArgs<Either<Integer, String>>>() { },
-                                         config)
-                    .get();
+            val value = random(new TypeToken<WithTypeArgs<Either<Integer, String>>>() { },
+                               config);
 
             value.t.apply(i -> assertThat(i).isNotZero(),
                           s -> assertThat(s).isNotNull());

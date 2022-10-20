f86682f76f9e22f85ba7a340c4715d6260497236
Abel Quiros <beluchin@gmail.com>
2022-10-20 12:18:06 +0100

    support for immutable maps - red

--- src/test/java/aq/rvg/GuavaCollectionsSupportTest.java
+++ src/test/java/aq/rvg/GuavaCollectionsSupportTest.java
@@ -1,10 +1,14 @@
 package aq.rvg;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.TypeToken;
 import lombok.val;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.function.Predicate;
 
 import static aq.rvg.Operational.random;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +21,22 @@ final class GuavaCollectionsSupportTest {
                 .isNotNull()
                 .isNotEmpty()
                 .allMatch(i -> i != 0);
+    }
+
+    @Test
+    void map() {
+        val keysAreNotZero = (Predicate<Map<Integer, String>>) m -> m.keySet().stream()
+                .allMatch(i -> i != 0);
+        val valuesAreNotEmpty = (Predicate<Map<Integer, String>>) m -> m.values().stream()
+                .noneMatch(String::isEmpty);
+
+        val value = random(new TypeToken<ImmutableMap<Integer, String>>() { });
+
+        assertThat(value)
+                .isNotNull()
+                .isNotEmpty()
+                .matches(keysAreNotZero)
+                .matches(valuesAreNotEmpty);
     }
 
     @Test
